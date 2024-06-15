6beea09677ee6e9a0841d5a2a37e2dfe37b38614
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-06-15 20:30:08 +0330

    add global colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,13 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        'redColor': '#f13a01',
+        'grayColor': '#6b7284'
+      }
+    }
+  },
 };
 export default config;
