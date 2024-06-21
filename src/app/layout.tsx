b30cd7048e9bf578cb0caac37517d0ec4b76e61c
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-06-21 18:44:22 +0330

    handle footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/large/Header";
 import "./app.css";
 import { Roboto } from "next/font/google";
+import Footer from "@/components/large/Footer";
 
 const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] });
 
@@ -16,11 +17,8 @@ export default function RootLayout({
         <div className="max-w-4xl mx-auto px-2 pt-5 text-grayColor">
           <Header />
           {children}
+          <Footer />
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
       </body>
     </html>
   );
---
+++ src/components/large/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="pt-8 pb-12 border-t text-center">
+            © 2023 All rights reserved
+        </footer>
+    )
+}
