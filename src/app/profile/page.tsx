aac5a2e23127657da74b731d0800e1a8f5b729c4
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-07-04 16:22:20 +0330

    coding profile page structure

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,33 @@
-export default function Profile(){
-    return (
-        <main>
-            <h1 className="text-6xl">Profile page</h1>
-        </main>
-    )
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <main className="mb-16">
+      {/* title */}
+      <h1 className="text-redColor text-4xl mt-8 text-center mb-10">Profile</h1>
+
+      {/* content */}
+      <div>
+        {/* left */}
+        <div>
+          {/* profile photo */}
+          <Image
+            src="/images/person.png"
+            alt="person profile image"
+            width="10000"
+            height="10000"
+          />
+          {/* edit button */}
+          <button type="button">Edit</button>
+        </div>
+        {/* right */}
+        <form>
+          {/* first and last name */}
+          <input type="text" placeholder="First and last name" />
+          <input type="text" />
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </main>
+  );
 }
