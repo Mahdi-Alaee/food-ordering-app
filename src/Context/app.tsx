f9fc38abcd475e11325bbf498abd2a71c5d91b45
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-08-02 19:21:53 +0330

    adapt cart with localstorage

--- src/Context/app.tsx
+++ src/Context/app.tsx
@@ -17,23 +17,35 @@ export function AppContextProvider({ children }: AppContextProviderProps) {
   const [cart, setCart] = useState<Cart[]>([]);
 
   useEffect(() => {
-    console.log({ cart });
-  }, [cart]);
+    const cartData = JSON.parse(localStorage.getItem("cart")!) as Cart[];
+    console.log({ cartData });
+    setCart(cartData || []);
+  }, []);
+
+  const setCartToLocalStorage = (cartData: Cart[]) =>
+    localStorage.setItem("cart", JSON.stringify(cartData));
 
   const addToCart = (newItem: Cart) => {
     console.log({ newItem });
 
     const isExists = cart.find((item) => item._id === newItem._id);
     console.log({ isExists });
-    if (!isExists) setCart((prev) => [...prev, newItem]);
+    if (!isExists)
+      setCart((prev) => {
+        const newCart = [...prev, newItem];
+        setCartToLocalStorage(newCart);
+        return newCart;
+      });
     else {
       console.log("in else");
 
-      setCart((prev) =>
-        prev.map((item) =>
+      setCart((prev) => {
+        const newCart = prev.map((item) =>
           item._id === newItem._id ? { ...item, count: item.count + 1 } : item
-        )
-      );
+        );
+        setCartToLocalStorage(newCart);
+        return newCart;
+      });
     }
   };
 
