7636a70f8e01e09d7ff9be0a7e6d8d7fdd215095
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-08-29 16:03:21 +0330

    create orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,17 @@
-export default function Orders(){
-    return (
-        <>
-            <h1>orders</h1>
-        </>
-    )
+'use client'
+
+import UserTabs from "@/components/medium/UserTabs";
+import useProfile from "@/hooks/useProfile";
+
+export default function Orders() {
+    const {isLoading,user} = useProfile()
+    if(isLoading)
+        return 'Loading...' 
+  return (
+    <main className="mb-16">
+      <UserTabs isAdmin={user?.isAdmin!} />
+
+      <h1>orders</h1>
+    </main>
+  );
 }
